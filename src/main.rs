dee1afcc7e298aa7379c9ceb0d0f4d605095795b
BKTales <47539420+BKTales@users.noreply.github.com>
2022-09-02 15:22:26 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+
+fn main() {
+    let num01 = 3.1;
+    let num02 = 2.1;
+    // How to get input?
+
+    fn add() -> f32{
+        return num01 + num02;
+    }
+    
+    fn sub() -> f32{
+        return num01 - num02;
+    }
+    
+    fn mult() -> f32{
+        return num01 * num02;
+    }
+    
+    fn div() -> f32{
+        return num01 / num02;
+    }
+}
+
+
