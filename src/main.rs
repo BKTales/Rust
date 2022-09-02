7651ecfb360bc07cf14fd632491e119b264e70b3
BKTales <47539420+BKTales@users.noreply.github.com>
2022-09-02 16:18:28 +0100

    Variables

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
-
 fn main() {
-    let num01 = 3.1;
+    let num01 = 5.2;
     let num02 = 2.1;
-    // How to get input?
 
-    fn add() -> f32{
-        return num01 + num02;
-    }
-    
-    fn sub() -> f32{
-        return num01 - num02;
-    }
-    
-    fn mult() -> f32{
-        return num01 * num02;
-    }
-    
-    fn div() -> f32{
-        return num01 / num02;
-    }
+    println!("{}", add(num01, num02));
+    println!("{}", sub(num01, num02));
+    println!("{}", mult(num01, num02));
+    println!("{}", div(num01, num02));
 }
 
+fn add(num01: f64, num02: f64) -> f64{
+    num01 + num02
+}
 
+fn sub(num01: f64, num02: f64) -> f64{
+    num01 - num02
+}
+
+fn mult(num01: f64, num02: f64) -> f64{
+    return num01 * num02;
+}
+
+fn div(num01: f64, num02: f64) -> f64{
+    return num01 / num02;
+}
+
+/*
+u -> Unsigned, which means, that the number can be '+', or neutral. (Integer)
+i -> Signed, which means, the number can be '+', '-' or neutral. (Integer)
+*/
